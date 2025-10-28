1bedd40381688633fc9a71c324b0cc45eb8ecaf2
rbqks529 <rbqks529@naver.com>
2025-10-28 12:36:06 +0900

    [refactor]: DialogPopup 버튼 텍스트 수정 (#150)

--- app/src/main/java/com/texthip/thip/ui/common/modal/DialogPopup.kt
+++ app/src/main/java/com/texthip/thip/ui/common/modal/DialogPopup.kt
@@ -4,9 +4,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,43 +26,46 @@ fun DialogPopup(
     modifier: Modifier = Modifier,
     title: String,
     description: String,
+    confirmText: String = stringResource(R.string.yes),
+    cancelText: String = stringResource(R.string.no),
     onConfirm: () -> Unit,
     onCancel: () -> Unit
 ) {
     Column(
         modifier = modifier
-            .size(width = 320.dp, height = 182.dp)
+            .width(320.dp)
             .background(
                 color = colors.DarkGrey,
                 shape = RoundedCornerShape(12.dp)
             )
-            .padding(20.dp),
-        verticalArrangement = Arrangement.SpaceBetween,
+            .padding(20.dp)
     ) {
         Text(
             text = title,
             color = colors.White,
             style = typography.smalltitle_m500_s18_h24,
         )
+        Spacer(modifier = Modifier.height(12.dp))
         Text(
             text = description,
             color = colors.White,
             style = typography.copy_r400_s14,
         )
+        Spacer(modifier = Modifier.height(20.dp))
 
         Row(
             horizontalArrangement = Arrangement.spacedBy(20.dp),
             modifier = Modifier.fillMaxWidth()
         ) {
             ActionMediumButton(
-                text = stringResource(R.string.no),
+                text = cancelText,
                 contentColor = colors.White,
                 backgroundColor = colors.Grey02,
                 modifier = Modifier.weight(1f),
                 onClick = onCancel,
             )
             ActionMediumButton(
-                text = stringResource(R.string.yes),
+                text = confirmText,
                 contentColor = colors.White,
                 backgroundColor = colors.Purple,
                 modifier = Modifier.weight(1f),
